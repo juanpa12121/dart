6843378a69f161073908f1f2381ff5a76c190b51
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2023-04-15 13:03:43 -0500

    Variables

--- bin/main.dart
+++ bin/main.dart
@@ -2,4 +2,37 @@ void main(List<String> arguments) {
   //Comentar una linea: ctrl + k + kc
   //Comentar bloque: alt + shift + a
   print('Hello world: !');
+  variables();
+
+}
+
+void variables(){
+
+  /* Numeros */
+  //Enteros
+  int numero = 3;
+  print(numero);
+
+  //Reales
+  double numeroReal = 3.14;
+  numeroReal = 9.99;
+  print(numeroReal);
+
+  //Texto
+  String nombre = 'Juan';
+  print(nombre);
+
+  String mensaje = "El resultado de la operacion es: $numeroReal";
+  print(mensaje);
+
+  String mensajeConSaltos=
+  """
+  Hola
+  Mundo
+  """;
+  print(mensajeConSaltos);
+
+  //Booleanos
+  bool activo = true;
+  print(activo);
 }
