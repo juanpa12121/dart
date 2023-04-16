8c76d633fa28ff8172b5f867cab3ce99dda80447
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2023-04-16 13:38:21 -0500

    Operadores 2

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,13 @@
+
+
 void main(List<String> arguments) {
   //Comentar una linea: ctrl + k + kc
   //Comentar bloque: alt + shift + a
   print('Hello world: !');
   //variables();
   //listas();
-  operadores();
+  //operadores();
+  sentenciasControlFlujo();
 }
 
 void variables() {
@@ -110,20 +113,73 @@ void variablesOtros() {
   const double inteligencia = 1.2;
 }
 
-void operadores(){
+void operadores() {
   //Operadores aritmeticos
   int a = 8;
   int b = 9;
 
-  int suma = a+b;
+  int suma = a + b;
   print(suma);
 
   //Operadores relacionales y de igualdad
-  print(1==2);
-  print(2<1);
+  print(1 == 2);
+  print(2 < 1);
 
   //Operadores lógicos: and, or
-  print(2==2 && 1==1);
-  print(1==1 || 3>5);
+  print(2 == 2 && 1 == 1);
+  print(1 == 1 || 3 > 5);
+}
 
+void sentenciasControlFlujo() {
+  final int initialLife = 100;
+  int currentLife = 100;
+
+  final attackEnemy = 50;
+
+  currentLife -= attackEnemy;
+  currentLife = currentLife - 5;
+
+  if (currentLife <= 0) {
+    print("Player is dead 😑");
+  } else if (currentLife > 0 && currentLife < initialLife) {
+    print("Player is alive but injured with $currentLife points of life 😣");
+  } else {
+    print("Player is alive");
+  }
+
+  //SWITCH
+  //Leer valor desde la consola
+  print("------------------------------");
+  var productoElegido = "hamburguesa";
+  switch (productoElegido) {
+    case "hamburguesa":
+      print("Has elegido una hamburguesa");
+      break;
+    case "pizza":
+      print("Has elegido una pizza");
+      break;
+    default:
+      print("No has elegido nada");
+  }
+
+  //BUCLE FOR
+  print("------------------------------");
+  var listaDeCompra = ['Leche', 'Huevos', 'Pan'];
+
+  for (var i = 0; i < listaDeCompra.length; i++) {
+    print(listaDeCompra[i]);
+  }
+  print("\n");
+  for (var producto in listaDeCompra) {
+    print(producto);
+  }
+
+  //WHILE
+  while (currentLife < initialLife) {
+    currentLife++;
+    print("Current life: $currentLife");
+  }
+
+  //TERNARIO
+  (1==1) ? print(true) : print(false);
 }
